28ac2da1935f0842009e0b745d7c30169fde471e
RomanZujev <roman.zujev@gmail.com>
2022-03-17 22:57:08 +0200

    added empty tab for timeslots passed, modified structureData() with further formatting functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 
-import createIcon, { correctArrayTimeslots } from "./functions.js";
+import createIcon, {
+  correctArrayTimeslots,
+  padArray,
+  convertTime,
+} from "./functions.js";
 export default function Header() {
   const [structuredData, setStructuredData] = useState([]);
   const [weather, setWeather] = useState([]);
@@ -36,19 +40,23 @@ export default function Header() {
     let month = data.init.slice(4, 6);
     let day = data.init.slice(6, 8);
     let time = data.init.slice(8);
-    //split array from weather data into 9 chunks (days)
+    time = convertTime(Number(time));
+    //assign proper timeslots
     let forecasts = correctArrayTimeslots(data.dataseries, Number(time));
+    //pad initial data if according to initial timeslot
+    forecasts = padArray(forecasts, Number(time));
 
-    forecasts = arrayOfArrays(forecasts, 9);
+    //split array from weather data into 8 chunks (days)
+    forecasts = arrayOfArrays(forecasts, 8);
     //for eacht day, create an object in structuredData,
     //containint date and forecast
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 7; i++) {
       //modify to account for month end-*-----------------------------------------------FUNCTION NEEDED----
-      day = Number(day) + 1;
       structuredData.push({
         date: `${day}.${month}.${year}`,
         forecast: forecasts[i],
       });
+      day = Number(day) + 1;
     }
     console.log(structuredData);
     setStructuredData(structuredData);
@@ -61,21 +69,34 @@ export default function Header() {
         return (
           <div key={index + "card"} className="fs-6 col-auto text-center">
             <div className="fw-bold bg-info">{element.date}</div>
-            {element.forecast.map((element, index) => (
-              <div key={index + "temp"} className="fs-6 col border">
-                <div className="row justify-content-center">
-                  {element.timepoint}
-                </div>{" "}
-                <img
-                  className="w-50 "
-                  src={createIcon(element.weather)}
-                  alt="icon"
-                />
-                <div className="row justify-content-center">
-                  {element.temp2m}°C
+            {element.forecast.map((element, index) => {
+              let tab;
+
+              let tab1 = (
+                <div key={index + "temp"} className="fs-6 col border">
+                  <div className="row justify-content-center">
+                    {element.timepoint}:00
+                  </div>{" "}
+                  <img
+                    className="w-50 "
+                    src={createIcon(element.weather)}
+                    alt="icon"
+                  />
+                  <div className="row justify-content-center">
+                    {element.temp2m}°C
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+              let tab2 = (
+                <div key={index + "temp"} className="fs-6 col border">
+                  <div className="row justify-content-center">-</div>{" "}
+                  <div className="row justify-content-center">-</div>
+                  <div className="row justify-content-center">-</div>
+                </div>
+              );
+              tab = element.cloudcover == null ? tab2 : tab1;
+              return tab;
+            })}
           </div>
         );
       })
