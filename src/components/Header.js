05267387ca2a0f1515847c177824d5eefc6733e8
RomanZujev <roman.zujev@gmail.com>
2022-03-18 20:56:45 +0200

    added description to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,8 @@
 export default function Header() {
-  return <header className="container bg-info fs-1 text">WEATHER</header>;
+  return (
+    <header className="container-fluid bg-info fs-1 text link-light">
+      WEATHER
+      <div className="fs-4">using 7timer weather API and GoogleMaps API</div>
+    </header>
+  );
 }
