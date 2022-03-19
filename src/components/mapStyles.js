f75b0ea1482e676ff2bb773a3c2aa39e52c3214a
RomanZujev <roman.zujev@gmail.com>
2022-03-19 19:36:57 +0200

    Json data for the googlemaps style settings

---
+++ src/components/mapStyles.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    featureType: "landscape",
+    stylers: [
+      {
+        color: "#0dcaf0",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    stylers: [
+      {
+        color: "#f0d860",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit.station",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    stylers: [
+      {
+        color: "#ffffff",
+      },
+    ],
+  },
+];
