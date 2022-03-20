27112c6fb8edfb3dc70d3a659bc5e03cb2f69382
RomanZujev <roman.zujev@gmail.com>
2022-03-20 18:22:40 +0200

    added an onClick event that produces latitude and longitude data

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,10 +2,9 @@ import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import mapStyles from "./mapStyles.js";
 
 const mapContainerStyle = { width: "48vw", height: "75vh" };
-const center = { lat: 60.169, lng: 24.938 };
 const options = { styles: mapStyles, disableDefaultUI: true };
 
-export default function App() {
+export default function Map(props) {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
@@ -18,12 +17,18 @@ export default function App() {
         <input placeholder="Input location:" className="" />
       </form>
       <div className="row justify-content-center fw-bold bg-light m-0 position-relative bg-warning link-info p-1 ">
-        WEATHER NOW
+        WEATHER NOW AT: {props.coordinates.lat}/{props.coordinates.lng}
       </div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={11}
-        center={center}
+        onClick={(event) => {
+          props.setCoordinates({
+            lat: Number(event.latLng.lat().toFixed(3)),
+            lng: Number(event.latLng.lng().toFixed(3)),
+          });
+        }}
+        center={props.coordinates}
         options={options}
       />
     </div>
