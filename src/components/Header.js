80fe151060270d1251c0cb915d1b45781f0b03c7
RomanZujev <roman.zujev@gmail.com>
2022-04-16 22:11:13 +0300

    edited header text to include yet another API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 export default function Header() {
+  console.log("Rendered HEADER");
   return (
     <header className="container-fluid bg-info fs-1 text link-light">
       WEATHER
-      <div className="fs-4">using 7timer weather API and GoogleMaps API</div>
+      <div className="fs-4">
+        using 7timer weather API, Geonames.org webservices-API and GoogleMaps
+        API
+      </div>
     </header>
   );
 }
