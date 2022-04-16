5a22f79b36e9570dd454186d672f56f989e77986
RomanZujev <roman.zujev@gmail.com>
2022-04-16 22:16:36 +0300

    moved functions from main, added default erro throws to functions with switches

--- src/components/functions.js
+++ src/components/functions.js
@@ -44,6 +44,8 @@ export function assignInitTimeSlot(initial) {
     case 1:
       result = 23;
       break;
+    default:
+      throw new Error("assignInitTimeSlot() got an unforseen value");
   }
   return result;
 }
@@ -74,6 +76,8 @@ export function assignNextTimeslot(initial) {
     case 23:
       result = 2;
       break;
+    default:
+      throw new Error("assignNextTimeslot() got an unforseen value");
   }
   return result;
 }
@@ -82,7 +86,7 @@ export function assignNextTimeslot(initial) {
 export function correctArrayTimeslots(array, time) {
   let arr = array.reduce((previous, current, index) => {
     //replace first elements timepoint value with json init time value
-    if (index == 0) {
+    if (index === 0) {
       current.timepoint = assignInitTimeSlot(time);
       return [...previous, current];
       //replace elements timepoint value with value based on the one before it
@@ -209,6 +213,10 @@ export function convertTime(valueGotten, timeAtLocation) {
       case 0:
         newValue = 24;
         break;
+      default:
+        throw new Error(
+          "convertTime got something other than a number between 0 and 11"
+        );
     }
   }
   return newValue;
@@ -221,4 +229,149 @@ export function arrayOfArrays(array, chunk) {
     temporary.push(array.slice(i, i + chunk));
   }
   return temporary;
+}
+export async function getAdminLocation(lat, lng) {
+  lat = lat.toString();
+  lng = lng.toString();
+  let user = process.env.REACT_APP_GEONAMES;
+  //web services
+  //https://www.geonames.org/export/web-services.html
+  //country and area
+  //http://api.geonames.org/countrySubdivision?lat=60.16&lng=24.9&radius=40&username=
+  const response = await fetch(
+    `http://api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lng}&username=${user}`
+  );
+  const data = await response.json();
+  let location = {
+    country: data.geonames[0].name,
+    adminArea: data.geonames[0].countryName,
+  };
+  return location;
+}
+
+//TESTING DATA:
+//STOCKHOLM
+//https://www.7timer.info/bin/civil.php?lon=18.00&lat=59.30&ac=0&lang=en&unit=metric&output=json
+//HELSINKI
+//https://www.7timer.info/bin/civil.php?lon=24.938&lat=60.169&ac=0&unit=metric&output=json
+//MOSCOW
+//https://www.7timer.info/bin/civil.php?lon=38.110&lat=55.785&ac=0&unit=metric&output=json
+
+//get weather data from API;
+export async function getWeatherData(lat, lng) {
+  const response = await fetch(
+    `https://www.7timer.info/bin/civil.php?lon=${lng}&lat=${lat}&ac=0&unit=metric&output=json`
+  );
+  const data = await response.json();
+  const stData = structureData(data);
+  return stData;
+}
+//get timezone offset from API;
+export async function getTimeZoneOffset(lat, lng) {
+  lat = lat.toString().slice(0, 3);
+  lng = lng.toString().slice(0, 3);
+  let user = process.env.REACT_APP_GEONAMES;
+  const response = await fetch(
+    `http://api.geonames.org/timezoneJSON?lat=${lat}&lng=${lng}&username=${user}`
+  );
+  const data = await response.json();
+
+  return data.gmtOffset;
+}
+//Create components from structured weather data
+export function createWeather(structuredData) {
+  let weatherArray = structuredData.map((element, index) => {
+    return (
+      //WEATHER COLUMN
+      <div key={index + "card"} className="fs-6 col text-center m-3 p-0">
+        {/*DATE cell*/}
+        <div className="row justify-content-center fw-bold bg-info small">
+          {element.date}
+        </div>
+        {element.forecast.map((element, index) => {
+          let tab;
+
+          let tab1 = (
+            //WEATHER DATA CELL
+            <div
+              key={index + "temp"}
+              className="fs-6 row border user-select-none"
+            >
+              <div className="col-6">
+                {/*TIME*/}
+                <div className="row small">{element.timepoint}:00</div>{" "}
+                {/*TEMPERATURE*/}
+                <div className="row small">{element.temp2m}°C</div>
+              </div>
+              {/*ICON*/}
+              <img
+                className="col-4 weatherIcon p-0"
+                src={createIcon(element.weather)}
+                alt="icon"
+              />
+            </div>
+          );
+          let tab2 = (
+            //WEATHER DATA CELL IF TIME HAS ALREADY PASSED
+            <div
+              key={index + "temp"}
+              className="fs-6 row border user-select-none bg-info opacity-25"
+            >
+              <div className="col-6">
+                {/*EMPTY TIME*/}
+                <div className="row small">.</div> {/*EMPTY TEMPERATURE*/}
+                <div className="row small">.</div>
+              </div>
+
+              {/*EMPTY ICON*/}
+              <img
+                className="col-4 weatherIcon p-0 "
+                src={createIcon(element.weather)}
+                alt="icon"
+              />
+            </div>
+          );
+          tab = element.cloudcover == null ? tab2 : tab1;
+          return tab;
+        })}
+      </div>
+    );
+  });
+  return weatherArray;
+}
+
+//takes api data and returns an array of objects containing
+//date object and forecast object of time+pemperateure objects
+export function structureData(data) {
+  let structuredData = [];
+  console.log("Got data with date code:" + data.init);
+  //let year = data.init.slice(0, 4);
+  let month = data.init.slice(4, 6);
+  let day = data.init.slice(6, 8);
+  let time = data.init.slice(8);
+  if (time === "00") {
+    time = "0";
+  }
+  /*----------------------------------------------------------------------------------------------------------------------FIX THIS HARDCODE*/
+  time = "1";
+  /*----------------------------------------------------------------------------------------------------------------------FIX THIS HARDCODE*/
+  time = convertTime(Number(time));
+  //assign proper timeslots
+  let forecasts = correctArrayTimeslots(data.dataseries, Number(time));
+  //pad initial data if according to initial timeslot
+  forecasts = padArray(forecasts, Number(time));
+
+  //split array from weather data into 8 chunks (days)
+  forecasts = arrayOfArrays(forecasts, 8);
+  //for eacht day, create an object in structuredData,
+  //containint date and forecast
+  for (let i = 0; i < 7; i++) {
+    //modify to account for month end-*-----------------------------------------------FUNCTION NEEDED----
+    structuredData.push({
+      date: `${day}.${month}`, //.${year}`,
+      forecast: forecasts[i],
+    });
+    day = Number(day) + 1;
+  }
+  return structuredData;
 }
